4fb15b4fc373625a4a03881c82f51e704e85c3dc
Ian Aranha <ianorama@gmail.com>
2019-07-25 22:41:18 -0700

    learning about forms and session

--- blogs/apps/blog_app/urls.py
+++ blogs/apps/blog_app/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.index),
-    url(r'^new$', views.new),
-    url(r'^create$', views.create),
-    url(r'^(?P<num>\d+)$', views.show),
-    url(r'^(?P<num>\d+)/edit$', views.edit),
-    url(r'^(?P<num>\d+)/delete$', views.destroy)
+    url(r'^blogs/new$', views.new),
+    url(r'^blogs/create$', views.create),
+    url(r'^blogs/(?P<num>\d+)$', views.show),
+    url(r'^blogs/(?P<num>\d+)/edit$', views.edit),
+    url(r'^blogs/(?P<num>\d+)/delete$', views.destroy)
 ]
+
+
--- blogs/apps/blog_app/views.py
+++ blogs/apps/blog_app/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 def index(request):
-    return HttpResponse('placeholder to later display all the list of blogs')
+    print('HELLO')
+    print(strftime("%Y-%m-%d    %H:%M" , gmtime()))
+    context ={
+        'email' : 'blog@email.com',
+        'name': "Ian"
+    }
+    return render(request,'blogs/index.html',context)
 
 def new(request):
     return HttpResponse('placeholder to display a new form to create a new blog')
 
 def create(request):
-    return redirect('/')
+    if request.method == 'POST':
+        print("*" * 50)
+        print(request.method)
+        print(request.POST['title'])
+        print(request.POST['description'])
+        request.session['name'] = request.POST['title']
+        return redirect('/')
+    else:
+        return redirect('/')
 
 def show(request, num):
     return HttpResponse('placeholder to display blog {}'.format(num))
