1f1ec96a1de075340686b12d2822bfa2ee582213
Ian Aranha <ianorama@gmail.com>
2019-07-26 15:24:58 -0700

    Added more functionality to app

--- main2/apps/random_word/views.py
+++ main2/apps/random_word/views.py
@@ -8,6 +8,14 @@ from django.utils.crypto import get_random_string
 COUNT = 0
 
 def index(request):
+    global COUNT
+    COUNT=0
+    Context={
+        'count': COUNT
+    }
+    return render (request, 'index.html', Context)
+
+def generate(request):
     r=get_random_string(length=15)
     global COUNT
     COUNT += 1
@@ -15,4 +23,4 @@ def index(request):
         'word': r.upper(),
         'count': COUNT
     }
-    return render (request, 'index.html', Context)
+    return render(request, 'index.html', Context)
