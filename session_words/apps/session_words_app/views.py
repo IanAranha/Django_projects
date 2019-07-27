5b71782b89e78322da8e963c32c779ae7de1b49d
Ian Aranha <ianorama@gmail.com>
2019-07-26 23:35:11 -0700

    Adding fully functional session_word app. Will add CSS tomorrow

---
+++ session_words/apps/session_words_app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, HttpResponse, redirect
+from time import strftime, localtime
+temp_list=[]
+# Create your views here.
+def index(request):
+    request.session.clear()
+    return render(request, 'index.html')
+
+def addWordToSession(request):
+    if request.method == 'POST':
+        wordtime = strftime('%H:%M:%S%p, %B %d %Y', localtime())
+        if 'font' in request.POST:
+            data = {
+                'word': request.POST['word'],
+                'color': request.POST['color'],
+                'font': '22px',
+                'time': wordtime
+            }
+        else:
+            data = {
+                'word': request.POST['word'],
+			    'color': request.POST['color'],
+			    'font': '16px',
+			    'time': wordtime
+            }
+        temp_list.append(data)
+        request.session['word'] = temp_list
+        return redirect('/show')
+
+def show(request):
+    context={
+        'data' : request.session['word']
+    }
+    return render(request,'index.html', context)
